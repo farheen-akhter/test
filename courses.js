1bc6370c4c8948f6d2cad90fcb8142821951992b
farheen-akhter <farheen.akhter@walkingtree.tech>
2019-11-05 17:04:19 +0530

    first commit

---
+++ courses.js
@@ -0,0 +1,189 @@
+import React from 'react';
+const Courses = () => {
+    return ( 
+        // <div>
+        //    Courses 
+        // </div>
+        <React.Fragment>
+                <div className="container" >
+                    <div className="header" >
+                        <div className="course" ><b>Courses</b></div>
+                        <div class="ui left icon input">
+                        <i aria-hidden="true" class="search link icon"></i>
+                            <input type="text" placeholder="Search..." />                            
+                        </div>
+                        <button class="ui positive button">                       
+                        <span className="plusIcon">&#10753;&nbsp;&nbsp;</span>
+                        Create New
+                        </button>
+                    </div>
+
+                    <div className="body" >
+       
+
+
+
+<div class="ui four column grid">
+<div class="column">
+<div class="ui segment">
+<div className="instructor-card">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul>
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="ui segment">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul>
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+<div class="column">
+<div class="ui segment">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul>
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+<div class="column">
+<div class="ui segment">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul>
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+<div class="column">
+<div class="ui segment">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul>
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+<div class="column">
+<div class="ui segment">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul>
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+<div class="column">
+<div class="ui segment">
+<div className="instructor-card">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul>
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+</div>
+<div class="column">
+<div class="ui segment">
+<div className="instructor-card">
+<div className="courseCard">
+<img src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/115565539/original/85df4df6bddad168eb52111276b817fb5c7944d9/fix-or-do-anything-in-html-css-javascript-bootstrap.png" alt="angular.png"></img>
+<h5>Web Techonology <span>&#xB7;&#xB7;&#xB7; 
+<div className="edit_delete">
+<ul className="list">
+<li>Edit</li>
+<li>Delete</li>
+</ul>
+</div>
+</span>
+</h5>
+<p>Course ID:</p>
+<p>Catogery:</p> 
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+                    </div>
+                </div>
+            </React.Fragment>
+     );
+}
+ 
+export default Courses;
